8a1e913271304158b0dea68b88f791cf8d325daf
Selcuk Tatar <41347478+tansionline@users.noreply.github.com>
2021-08-21 19:16:44 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,4 @@ apt update;
 apt install docker.io -y;
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose;
 sudo chmod +x /usr/local/bin/docker-compose;
-git clone https://github.com/tansionline/docker-elk.git;
-cd docker-elk/;
 docker-compose up -d;
